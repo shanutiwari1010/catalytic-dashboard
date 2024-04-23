bbda3b39ac6fa0f83878bd74b4b015f78a63e396
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-24 03:10:36 +0530

    feat: added search bar

---
+++ components/Charts/Explorer.jsx
@@ -0,0 +1,14 @@
+function Explorer() {
+  return (
+    <div className="flex justify-evenly gap-5 text-white border-y-2 border-sky-700 mt-5 p-3">
+      {/* <SearchBar /> */}
+      {/* <SelectStatus /> */}
+      <div className="flex gap-4">
+        <h2>Last Event</h2>
+        <p>theft/heatbeat/etc</p>
+      </div>
+    </div>
+  );
+}
+
+export default Explorer;
