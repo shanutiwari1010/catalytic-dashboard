0b686033faadc4f9421178228a9784594aaa171a
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-24 09:28:43 +0530

    feat: added intheft page

--- pages/intheft/index.jsx
+++ pages/intheft/index.jsx
@@ -1,5 +1,22 @@
+import { Button } from "@/components/ui/button";
+
 function InTheft() {
-  return <div>theft</div>;
+  return (
+    <div
+      className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+      x-chunk="dashboard-02-chunk-1"
+    >
+      <div className="flex flex-col items-center gap-1 text-center">
+        <h3 className="text-2xl font-bold tracking-tight">
+          You have no in-thefts
+        </h3>
+        <p className="text-sm text-muted-foreground">
+          You can start review as soon as you add a device.
+        </p>
+        <Button className="mt-4">Add Device</Button>
+      </div>
+    </div>
+  );
 }
 
 export default InTheft;
