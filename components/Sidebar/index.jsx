5a9185c959de3cdbfea017dc8bbcf5e85f048b9d
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-24 17:44:07 +0530

    feat: added sidebar component

---
+++ components/Sidebar/index.jsx
@@ -0,0 +1,69 @@
+import { Link } from "react-router-dom";
+import {
+  Package2,
+  Bell,
+  Home,
+  TriangleAlert,
+  Package,
+  Car,
+  Users,
+} from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+function Sidebar() {
+  return (
+    <div className="flex h-full max-h-screen flex-col gap-2">
+      <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <Link to="/" className="flex items-center gap-2 font-semibold">
+          <Package2 className="h-6 w-6" />
+          <span className="">Logo name</span>
+        </Link>
+        <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+          <Bell className="h-4 w-4" />
+          <span className="sr-only">Toggle notifications</span>
+        </Button>
+      </div>
+      <div className="flex-1">
+        <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+          <Link
+            to="/"
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          >
+            <Home className="h-4 w-4" />
+            Dashboard
+          </Link>
+          <Link
+            to="/intheft"
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          >
+            <TriangleAlert className="h-4 w-4" />
+            inTheft
+          </Link>
+          <Link
+            to="/devices"
+            className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
+          >
+            <Package className="h-4 w-4" />
+            Devices
+          </Link>
+          <Link
+            to="/vehicles"
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          >
+            <Car className="h-4 w-4" />
+            Vehicles
+          </Link>
+          <Link
+            to="/customers"
+            className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+          >
+            <Users className="h-4 w-4" />
+            Customers
+          </Link>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
