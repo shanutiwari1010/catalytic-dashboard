ba393f806f734bf591a602b4bbc5a8d747d4c65f
Shanu Tiwari <shanutiwari.work@gmail.com>
2024-04-26 13:20:54 +0530

    style: make the sidebar fixed

--- components/Sidebar/index.jsx
+++ components/Sidebar/index.jsx
@@ -15,7 +15,7 @@ import { Button } from "@/components/ui/button";
 
 function Sidebar() {
   return (
-    <div className="flex h-full max-h-screen flex-col gap-2">
+    <div className="fixed flex h-full max-h-screen flex-col gap-2">
       <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
         <Link to="/" className="flex items-center gap-2 font-semibold">
           <Package2 className="h-6 w-6" />
